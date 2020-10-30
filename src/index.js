66e3a0522969712291c357c506c1b08e3ba6b32f
Italo Fernandes <italojafernandes@hotmail.com>
2020-10-30 01:04:32 -0300

    Botao e funcao adicionar novo projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -11,6 +11,18 @@ export default function App() {
       setProjects(response.data);
     });
   }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto mobile ${Date.now()}`,
+      owner: 'Italo Fernandes'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return(
     <>
       <StatusBar backgroundColor="#7159c1"></StatusBar>
@@ -21,6 +33,10 @@ export default function App() {
         renderItem={({ item: project }) => (
           <Text style={styles.title}>{project.title}</Text>
         )} />
+
+        <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   )
@@ -36,4 +52,18 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 20,
   },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });
